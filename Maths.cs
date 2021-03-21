9564d47a4570e65dd7a7f486a166e652c11541d3
RealABlair <75645403+RealABlair@users.noreply.github.com>
2021-03-21 14:41:58 +0300

    Update Maths.cs

--- Maths.cs
+++ Maths.cs
@@ -8,6 +8,34 @@ namespace ABSoftware
 {
     public class Maths
     {
+        public static string ToHex(int number)
+        {
+            string hex = "";
+            double lastDiv = number;
+            while((int)lastDiv != 0)
+            {
+                double div = number / 16;
+                double rem = div - (int)div;
+                int result = (int)div;
+                hex += HEX_CHARS((int)(rem * 16));
+            }
+            char[] array = hex.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+
+        public static int ToInt(string hex)
+        {
+            char[] array = hex.ToCharArray();
+            int sum = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                int byt = INT_FROM_HEX(array[i]);
+                sum += sum * PowerNumber(sum, i);
+            }
+            return sum;
+        }
+
         public static int PowerNumber(int num, int power)
         {
             for (int i = 1; i < power; i++)
@@ -49,6 +77,86 @@ namespace ABSoftware
             {
                 return Lerp(start, end, (float)rnd.NextDouble());
             }
+        }
+
+        private static char HEX_CHARS(int integer)
+        {
+            switch(integer)
+            {
+                case 0:
+                    return '0';
+                case 1:
+                    return '1';
+                case 2:
+                    return '2';
+                case 3:
+                    return '3';
+                case 4:
+                    return '4';
+                case 5:
+                    return '5';
+                case 6:
+                    return '6';
+                case 7:
+                    return '7';
+                case 8:
+                    return '8';
+                case 9:
+                    return '9';
+                case 10:
+                    return 'A';
+                case 11:
+                    return 'B';
+                case 12:
+                    return 'C';
+                case 13:
+                    return 'D';
+                case 14:
+                    return 'E';
+                case 15:
+                    return 'F';
+            }
+            return '0';
+        }
+
+        private static int INT_FROM_HEX(char hex)
+        {
+            switch (hex)
+            {
+                case '0':
+                    return 0;
+                case '1':
+                    return 1;
+                case '2':
+                    return 2;
+                case '3':
+                    return 3;
+                case '4':
+                    return 4;
+                case '5':
+                    return 5;
+                case '6':
+                    return 6;
+                case '7':
+                    return 7;
+                case '8':
+                    return 8;
+                case '9':
+                    return 9;
+                case 'A':
+                    return 10;
+                case 'B':
+                    return 11;
+                case 'C':
+                    return 12;
+                case 'D':
+                    return 13;
+                case 'E':
+                    return 14;
+                case 'F':
+                    return 15;
+            }
+            return 0;
         }
     }
 }
