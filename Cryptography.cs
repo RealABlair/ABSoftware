ee2f27093f0169c5a95452c4c9e233c8617f7544
RealABlair <75645403+RealABlair@users.noreply.github.com>
2021-06-05 14:17:50 +0300

    Add files via upload

---
+++ Cryptography.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hasher.ABSoftware
+{
+    public class Cryptography
+    {
+        public static string Hash(string text)
+        {
+            ulong hash = 0;
+            for (var i = 0; i < text.Length; i++)
+            {
+                hash += (ulong)(text[i] + 1) * (ulong)(Math.Sqrt(hash) + 9);
+            }
+            hash += (ulong)text.Length;
+            if (hash < ulong.MaxValue / 10)
+                hash *= hash / (ulong)text.Length;
+            return hash.ToString("X16");
+        }
+    }
+}
