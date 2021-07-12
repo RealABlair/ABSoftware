1cb638ae1c6c404fa60ed3fb21bf2f1170ae5eae
RealABlair <75645403+RealABlair@users.noreply.github.com>
2021-07-13 00:34:39 +0300

    Update Network.cs

--- NeuralNetwork/Network.cs
+++ NeuralNetwork/Network.cs
@@ -1,161 +1,178 @@
-﻿using System;
 using System.Collections.Generic;
+using System;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ABSoftware.NeuralNetwork
+public class Network : IComparable<NeuralNetwork>
 {
-    public class Network
+    private int[] layers;//layers
+    private float[][] neurons;//neurons
+    private float[][] biases;//biasses
+    private float[][][] weights;//weights
+    private int[] activations;//layers
+
+    public float fitness = 0;//fitness
+
+    public Network(int[] layers)
     {
-        private int[] layers;
-        private float[][] neurons;
-        private float[][] biases;
-        private float[][][] weights;
-        private int[] activations;
-
-        public float fitness = 0f;
-
-        public Network(int[] layers)
+        this.layers = new int[layers.Length];
+        for (int i = 0; i < layers.Length; i++)
         {
-            this.layers = new int[layers.Length];
-            for(int i = 0; i < layers.Length; i++)
-            {
-                this.layers[i] = layers[i];
-            }
-            InitNeurons();
-            InitBiases();
-            InitWeights();
+            this.layers[i] = layers[i];
         }
+        InitNeurons();
+        InitBiases();
+        InitWeights();
+    }
 
-        private void InitNeurons()
+    private void InitNeurons()//create empty storage array for the neurons in the network.
+    {
+        List<float[]> neuronsList = new List<float[]>();
+        for (int i = 0; i < layers.Length; i++)
         {
-            List<float[]> neuronsList = new List<float[]>();
-            for(int i = 0; i < layers.Length; i++)
-            {
-                neuronsList.Add(new float[layers[i]]);
-            }
-            neurons = neuronsList.ToArray();
+            neuronsList.Add(new float[layers[i]]);
         }
+        neurons = neuronsList.ToArray();
+    }
 
-        private void InitBiases()
+    private void InitBiases()//initializes and populates array for the biases being held within the network.
+    {
+        List<float[]> biasList = new List<float[]>();
+        for (int i = 0; i < layers.Length; i++)
         {
-            List<float[]> biasList = new List<float[]>();
-            for (int i = 0; i < layers.Length; i++)
+            float[] bias = new float[layers[i]];
+            for (int j = 0; j < layers[i]; j++)
             {
-                float[] bias = new float[layers[i]];
-                for (int j = 0; j < layers[i]; j++)
+                bias[j] = UnityEngine.Random.Range(-0.5f, 0.5f);
+            }
+            biasList.Add(bias);
+        }
+        biases = biasList.ToArray();
+    }
+
+    private void InitWeights()//initializes random array for the weights being held in the network.
+    {
+        List<float[][]> weightsList = new List<float[][]>();
+        for (int i = 1; i < layers.Length; i++)
+        {
+            List<float[]> layerWeightsList = new List<float[]>();
+            int neuronsInPreviousLayer = layers[i - 1];
+            for (int j = 0; j < neurons[i].Length; j++)
+            {
+                float[] neuronWeights = new float[neuronsInPreviousLayer];
+                for (int k = 0; k < neuronsInPreviousLayer; k++)
                 {
-                    bias[j] = Maths.Random.Range(-0.5f, 0.5f);
+                    //float sd = 1f / ((neurons[i].Length + neuronsInPreviousLayer) / 2f);
+                    neuronWeights[k] = UnityEngine.Random.Range(-0.5f, 0.5f);
                 }
-                biasList.Add(bias);
+                layerWeightsList.Add(neuronWeights);
             }
-            biases = biasList.ToArray();
+            weightsList.Add(layerWeightsList.ToArray());
         }
+        weights = weightsList.ToArray();
+    }
 
-        private void InitWeights()
+    public float[] FeedForward(float[] inputs)//feed forward, inputs >==> outputs.
+    {
+        for (int i = 0; i < inputs.Length; i++)
         {
-            List<float[][]> weightsList = new List<float[][]>();
-            for (int i = 1; i < layers.Length; i++)
+            neurons[0][i] = inputs[i];
+        }
+        for (int i = 1; i < layers.Length; i++)
+        {
+            int layer = i - 1;
+            for (int j = 0; j < neurons[i].Length; j++)
             {
-                List<float[]> layerWeightsList = new List<float[]>();
-                int neuronsInPreviousLayer = layers[i - 1];
-                for (int j = 0; j < neurons[i].Length; j++)
+                float value = 0f;
+                for (int k = 0; k < neurons[i - 1].Length; k++)
                 {
-                    float[] neuronWeights = new float[neuronsInPreviousLayer];
-                    for (int k = 0; k < neuronsInPreviousLayer; k++)
-                    {
-                        neuronWeights[k] = Maths.Random.Range(-0.5f, 0.5f);
-                    }
-                    layerWeightsList.Add(neuronWeights);
+                    value += weights[i - 1][j][k] * neurons[i - 1][k];
                 }
-                weightsList.Add(layerWeightsList.ToArray());
+                neurons[i][j] = activate(value + biases[i][j]);
             }
-            weights = weightsList.ToArray();
+        }
+        return neurons[neurons.Length - 1];
+    }
+
+    public float activate(float value)
+    {
+        return (float)Math.Tanh(value);
+    }
+
+    public void Mutate(int chance, float val)//used as a simple mutation function for any genetic implementations.
+    {
+        for (int i = 0; i < biases.Length; i++)
+        {
+            for (int j = 0; j < biases[i].Length; j++)
+            {
+                biases[i][j] = (UnityEngine.Random.Range(0f, chance) <= 5) ? biases[i][j] += UnityEngine.Random.Range(-val, val) : biases[i][j];
+            }
         }
 
-        public float activate(float value)
+        for (int i = 0; i < weights.Length; i++)
         {
-            return (float)Math.Tanh(value);
-        }
-
-        public float[] FeedForward(float[] inputs)
-        {
-            for (int i = 0; i < inputs.Length; i++)
+            for (int j = 0; j < weights[i].Length; j++)
             {
-                neurons[0][i] = inputs[i];
-            }
-            for (int i = 1; i < layers.Length; i++)
-            {
-                int layer = i - 1;
-                for (int j = 0; j < neurons[i].Length; j++)
+                for (int k = 0; k < weights[i][j].Length; k++)
                 {
-                    float value = 0f;
-                    for (int k = 0; k < neurons[i - 1].Length; k++)
-                    {
-                        value += weights[i - 1][j][k] * neurons[i - 1][k];
-                    }
-                    neurons[i][j] = activate(value + biases[i][j]);
-                }
-            }
-            return neurons[neurons.Length - 1];
-        }
-
-        public int CompareTo(Network other)
-        {
-            if (other == null)
-                return 1;
-            if (fitness > other.fitness)
-                return 1;
-            else if (fitness < other.fitness)
-                return -1;
-            else
-                return 0;
-        }
-
-        public void Load(string path)
-        {
-            TextReader tr = new StreamReader(path);
-            int NumberOfLines = (int)new FileInfo(path).Length;
-            string[] ListLines = new string[NumberOfLines];
-            int index = 1;
-            for (int i = 1; i < NumberOfLines; i++)
-            {
-                ListLines[i] = tr.ReadLine();
-            }
-            tr.Close();
-            if (new FileInfo(path).Length > 0)
-            {
-                for (int i = 0; i < biases.Length; i++)
-                {
-                    for (int j = 0; j < biases[i].Length; j++)
-                    {
-                        biases[i][j] = float.Parse(ListLines[index]);
-                        index++;
-                    }
-                }
-                for (int i = 0; i < weights.Length; i++)
-                {
-                    for (int j = 0; j < weights[i].Length; j++)
-                    {
-                        for (int k = 0; k < weights[i][j].Length; k++)
-                        {
-                            weights[i][j][k] = float.Parse(ListLines[index]);
-                            index++;
-                        }
-                    }
+                    weights[i][j][k] = (UnityEngine.Random.Range(0f, chance) <= 5) ? weights[i][j][k] += UnityEngine.Random.Range(-val, val) : weights[i][j][k];
                 }
             }
         }
+    }
 
-        public void Mutate(int chance, float val)
+    public int CompareTo(Network other) //Comparing For NeuralNetworks performance.
+    {
+        if (other == null) return 1;
+
+        if (fitness > other.fitness)
+            return 1;
+        else if (fitness < other.fitness)
+            return -1;
+        else
+            return 0;
+    }
+
+    public Network copy(NeuralNetwork nn) //For creatinga deep copy, to ensure arrays are serialzed.
+    {
+        for (int i = 0; i < biases.Length; i++)
+        {
+            for (int j = 0; j < biases[i].Length; j++)
+            {
+                nn.biases[i][j] = biases[i][j];
+            }
+        }
+        for (int i = 0; i < weights.Length; i++)
+        {
+            for (int j = 0; j < weights[i].Length; j++)
+            {
+                for (int k = 0; k < weights[i][j].Length; k++)
+                {
+                    nn.weights[i][j][k] = weights[i][j][k];
+                }
+            }
+        }
+        return nn;
+    }
+
+    public void Load(string path)//this loads the biases and weights from within a file into the neural network.
+    {
+        TextReader tr = new StreamReader(path);
+        int NumberOfLines = (int)new FileInfo(path).Length;
+        string[] ListLines = new string[NumberOfLines];
+        int index = 1;
+        for (int i = 1; i < NumberOfLines; i++)
+        {
+            ListLines[i] = tr.ReadLine();
+        }
+        tr.Close();
+        if (new FileInfo(path).Length > 0)
         {
             for (int i = 0; i < biases.Length; i++)
             {
                 for (int j = 0; j < biases[i].Length; j++)
                 {
-                    biases[i][j] = (Maths.Random.Range(0f, chance) <= 5) ? biases[i][j] += Maths.Random.Range(-val, val) : biases[i][j];
+                    biases[i][j] = float.Parse(ListLines[index]);
+                    index++;
                 }
             }
 
@@ -165,11 +182,37 @@ namespace ABSoftware.NeuralNetwork
                 {
                     for (int k = 0; k < weights[i][j].Length; k++)
                     {
-                        weights[i][j][k] = (Maths.Random.Range(0f, chance) <= 5) ? weights[i][j][k] += Maths.Random.Range(-val, val) : weights[i][j][k];
-
+                        weights[i][j][k] = float.Parse(ListLines[index]); ;
+                        index++;
                     }
                 }
             }
         }
+    }
+
+    public void Save(string path)//this is used for saving the biases and weights within the network to a file.
+    {
+        File.Create(path).Close();
+        StreamWriter writer = new StreamWriter(path, true);
+
+        for (int i = 0; i < biases.Length; i++)
+        {
+            for (int j = 0; j < biases[i].Length; j++)
+            {
+                writer.WriteLine(biases[i][j]);
+            }
+        }
+
+        for (int i = 0; i < weights.Length; i++)
+        {
+            for (int j = 0; j < weights[i].Length; j++)
+            {
+                for (int k = 0; k < weights[i][j].Length; k++)
+                {
+                    writer.WriteLine(weights[i][j][k]);
+                }
+            }
+        }
+        writer.Close();
     }
 }
