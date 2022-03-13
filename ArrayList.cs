b28f0d4c2db856f95cb704b0d18349c623abf7f0
RealABlair <75645403+RealABlair@users.noreply.github.com>
2022-03-13 16:08:59 +0300

    Add files via upload

---
+++ ArrayList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABSoftware
+{
+    public class ArrayList<T>
+    {
+        T[] elements = null;
+
+        public int Size { get { return elements.Length; } }
+
+        public ArrayList()
+        {
+            this.elements = new T[0];
+        }
+
+        public ArrayList(T[] elements)
+        {
+            this.elements = elements;
+        }
+
+        public T[] GetElements()
+        {
+            return elements;
+        }
+
+        public T Get(int id)
+        {
+            return elements[id];
+        }
+
+        public void Add(T element)
+        {
+            Array.Resize(ref elements, elements.Length + 1);
+            elements[elements.Length - 1] = element;
+        }
+
+        public void Remove(T element)
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (elements[i].Equals(element))
+                {
+                    RemoveAt(i);
+                    break;
+                }
+            }
+        }
+
+        public void RemoveAt(int id)
+        {
+            elements = elements.Where((source, index) => index != id).ToArray();
+        }
+
+        public void Clear()
+        {
+            elements = new T[0];
+        }
+    }
+}
