10c15cd3f33669a81c88a1c992682721982949c9
RealABlair <75645403+RealABlair@users.noreply.github.com>
2022-04-27 02:08:09 +0300

    Update KLINToken.cs

--- KLINToken.cs
+++ KLINToken.cs
@@ -1,19 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace ABSoftware
 {
     public class KLINToken
     {
-        public string Property { get; set; }
-        public object value { get; set; }
+        public string PropertyName { get; set; }
+        public object Value { get; set; }
+
+        public KLINToken(string PropertyName = null, object Value = null)
+        {
+            this.PropertyName = PropertyName;
+            this.Value = Value;
+        }
 
         public override string ToString()
         {
-            return value.ToString();
+            return Value.ToString();
         }
     }
 }
