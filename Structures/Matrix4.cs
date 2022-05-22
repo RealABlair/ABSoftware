8e085c098079b4c63d3428c3a4e5fe52134d9308
RealABlair <75645403+RealABlair@users.noreply.github.com>
2022-05-22 16:43:46 +0300

    Add files via upload

---
+++ Structures/Matrix4.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace ABSoftware.Structures
+{
+    public class Matrix4
+    {
+        public float[,] m = new float[4, 4];
+
+        public Vector3 MultiplyVector(Vector3 a)
+        {
+            Vector3 v = new Vector3();
+            v.x = a.x * m[0, 0] + a.y * m[1, 0] + a.z * m[2, 0] + a.w * m[3, 0];
+            v.y = a.x * m[0, 1] + a.y * m[1, 1] + a.z * m[2, 1] + a.w * m[3, 1];
+            v.z = a.x * m[0, 2] + a.y * m[1, 2] + a.z * m[2, 2] + a.w * m[3, 2];
+            v.w = a.x * m[0, 3] + a.y * m[1, 3] + a.z * m[2, 3] + a.w * m[3, 3];
+            return v;
+        }
+
+        public void MakeIdentity()
+        {
+            m[0, 0] = 1f;
+            m[1, 1] = 1f;
+            m[2, 2] = 1f;
+            m[3, 3] = 1f;
+        }
+
+        public void RotationX(float RadAngle)
+        {
+            m[0, 0] = 1f;
+            m[1, 1] = (float)Math.Cos(RadAngle);
+            m[1, 2] = (float)Math.Sin(RadAngle);
+            m[2, 1] = (float)-Math.Sin(RadAngle);
+            m[2, 2] = (float)Math.Cos(RadAngle);
+            m[3, 3] = 1f;
+        }
+
+        public void RotationY(float RadAngle)
+        {
+            m[0, 0] = (float)Math.Cos(RadAngle);
+            m[2, 0] = (float)Math.Sin(RadAngle);
+            m[1, 1] = 1f;
+            m[0, 2] = (float)-Math.Sin(RadAngle);
+            m[2, 2] = (float)Math.Cos(RadAngle);
+            m[3, 3] = 1f;
+        }
+
+        public void RotationZ(float RadAngle)
+        {
+            m[0, 0] = (float)Math.Cos(RadAngle);
+            m[0, 1] = (float)Math.Sin(RadAngle);
+            m[1, 0] = (float)-Math.Sin(RadAngle);
+            m[1, 1] = (float)Math.Cos(RadAngle);
+            m[2, 2] = 1f;
+            m[3, 3] = 1f;
+        }
+
+        public void Translation(float x, float y, float z)
+        {
+            m[0, 0] = 1.0f;
+            m[1, 1] = 1.0f;
+            m[2, 2] = 1.0f;
+            m[3, 3] = 1.0f;
+            m[3, 0] = x;
+            m[3, 1] = y;
+            m[3, 2] = z;
+        }
+
+        public void Projection(float fov, float aspectRatio, float near, float far)
+        {
+            float fovRad = 1.0f / (float)Math.Tan(fov * 0.5f / 180.0f * Math.PI);
+            m[0, 0] = aspectRatio * fovRad;
+            m[1, 1] = fovRad;
+            m[2, 2] = far / (far - near);
+            m[3, 2] = (-far * near) / (far - near);
+            m[2, 3] = 1.0f;
+            m[3, 3] = 0.0f;
+        }
+
+        public Matrix4 MultiplyMatrix(Matrix4 a, Matrix4 b)
+        {
+            Matrix4 newMat = new Matrix4();
+            for(int c = 0; c < 4; c++)
+                for (int r = 0; r < 4; r++)
+                    newMat.m[r, c] = a.m[r, 0] * b.m[0, c] + a.m[r, 1] * b.m[1, c] + a.m[r, 2] * b.m[2, c] + a.m[r, 3] * b.m[3, c];
+            return newMat;
+        }
+
+        public Vector3 MultiplyMatrixVector(Vector3 i)
+        {
+            Vector3 o = new Vector3();
+            o.x = i.x * m[0, 0] + i.y * m[1, 0] + i.z * m[2, 0] + m[3, 0];
+            o.y = i.x * m[0, 1] + i.y * m[1, 1] + i.z * m[2, 1] + m[3, 1];
+            o.z = i.x * m[0, 2] + i.y * m[1, 2] + i.z * m[2, 2] + m[3, 2];
+            float w = i.x * m[0, 3] + i.y * m[1, 3] + i.z * m[2, 3] + m[3, 3];
+
+            if (w != 0.0f)
+            {
+                o.x /= w; o.y /= w; o.z /= w;
+            }
+
+            return o;
+        }
+    }
+}
---
+++ Structures/Vector3.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace ABSoftware.Structures
+{
+    public class Vector3
+    {
+        public float x;
+        public float y;
+        public float z;
+        public float w;
+
+        public Vector3(float x = 0f, float y = 0f, float z = 0f, float w = 1f)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
+        }
+
+        public float Distance(Vector3 to)
+        {
+            float x = Math.Max(this.x, to.x) - Math.Min(this.x, to.x);
+            float y = Math.Max(this.y, to.y) - Math.Min(this.y, to.y);
+            float z = Math.Max(this.z, to.z) - Math.Min(this.z, to.z);
+
+            return (float)Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2));
+        }
+
+        public void Cross(Vector3 a, Vector3 b)
+        {
+            this.x = a.y * b.z - a.z * b.y;
+            this.y = a.z * b.x - a.x * b.z;
+            this.z = a.x * b.y - a.y * b.x;
+        }
+
+        public void MoveHorizontally(float radians, float distance)
+        {
+            this.x = (float)Math.Sin(radians) * distance;
+            this.z = (float)Math.Cos(radians) * distance;
+        }
+
+        public float DotProduct(Vector3 a, Vector3 b)
+        {
+            return a.x * b.x + a.y * b.y + a.z * b.z;
+        }
+
+        public float Length()
+        {
+            return (float)Math.Sqrt(x * x + y * y + z * z);
+        }
+
+        public void Normalize()
+        {
+            float l = Length();
+
+            x /= l;
+            y /= l;
+            z /= l;
+        }
+
+        public void Sub(Vector3 a, Vector3 b)
+        {
+            x = a.x - b.x;
+            y = a.y - b.y;
+            z = a.z - b.z;
+        }
+
+        public void Add(Vector3 a, Vector3 b)
+        {
+            x = a.x + b.x;
+            y = a.y + b.y;
+            z = a.z + b.z;
+        }
+
+        public void Div(Vector3 a, float b)
+        {
+            x = a.x / b;
+            y = a.y / b;
+            z = a.z / b;
+        }
+
+        public void Mul(Vector3 a, float b)
+        {
+            x = a.x * b;
+            y = a.y * b;
+            z = a.z * b;
+        }
+
+        public static Vector3 operator + (Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.x + b.x, a.y + b.y, a.z + b.z);
+        }
+
+        public static Vector3 operator - (Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.x - b.x, a.y - b.y, a.z - b.z);
+        }
+
+        public static Vector3 operator / (Vector3 a, float b)
+        {
+            return new Vector3(a.x / b, a.y / b, a.z / b);
+        }
+
+        public static Vector3 operator * (Vector3 a, float b)
+        {
+            return new Vector3(a.x * b, a.y * b, a.z * b);
+        }
+
+        public override string ToString()
+        {
+            return "x:" + x + ", " + "y:" + y + ", " + "z:" + z;
+        }
+    }
+}
