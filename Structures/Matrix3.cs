2185ac3b3649fcf5e488319c6af158983666a056
RealABlair <75645403+RealABlair@users.noreply.github.com>
2022-05-22 16:40:10 +0300

    Add files via upload

---
+++ Structures/Matrix3.cs
@@ -0,0 +1,200 @@
+﻿using System;
+
+namespace ABSoftware.Structures
+{
+    public class Matrix3
+    {
+        public float[,] m = new float[3, 3];
+
+        public Matrix3()
+        {
+
+        }
+
+        public Matrix3(float[,] m)
+        {
+            this.m = m;
+        }
+
+        public void Identity()
+        {
+            m[0,0] = 1.0f; m[1,0] = 0.0f; m[2,0] = 0.0f;
+            m[0,1] = 0.0f; m[1,1] = 1.0f; m[2,1] = 0.0f;
+            m[0,2] = 0.0f; m[1,2] = 0.0f; m[2,2] = 1.0f;
+        }
+
+        public void Translate(float x, float y)
+        {
+            m[0,0] = 1.0f; m[1,0] = 0.0f; m[2,0] = x;
+            m[0,1] = 0.0f; m[1,1] = 1.0f; m[2,1] = y;
+            m[0,2] = 0.0f; m[1,2] = 0.0f; m[2,2] = 1.0f;
+        }
+
+        public void Rotate(float RadAngle)
+        {
+            m[0,0] = (float)Math.Cos(RadAngle); m[1,0] = (float)Math.Sin(RadAngle); m[2,0] = 0.0f;
+            m[0,1] = -(float)Math.Sin(RadAngle); m[1,1] = (float)Math.Cos(RadAngle); m[2,1] = 0.0f;
+            m[0,2] = 0.0f; m[1,2] = 0.0f; m[2,2] = 1.0f;
+        }
+
+        public void Scale(float x, float y)
+        {
+            m[0,0] = x; m[1,0] = 0.0f; m[2,0] = 0.0f;
+            m[0,1] = 0.0f; m[1,1] = y; m[2,1] = 0.0f;
+            m[0,2] = 0.0f; m[1,2] = 0.0f; m[2,2] = 1.0f;
+        }
+
+        public void Shear(float x, float y)
+        {
+            m[0,0] = 1.0f; m[1,0] = x; m[2,0] = 0.0f;
+            m[0,1] = y; m[1,1] = 1.0f; m[2,1] = 0.0f;
+            m[0,2] = 0.0f; m[1,2] = 0.0f; m[2,2] = 1.0f;
+        }
+
+        public Matrix3 Identity_M()
+        {
+            Matrix3 ma = new Matrix3();
+            ma.m[0, 0] = 1.0f; ma.m[1, 0] = 0.0f; ma.m[2, 0] = 0.0f;
+            ma.m[0, 1] = 0.0f; ma.m[1, 1] = 1.0f; ma.m[2, 1] = 0.0f;
+            ma.m[0, 2] = 0.0f; ma.m[1, 2] = 0.0f; ma.m[2, 2] = 1.0f;
+            return ma;
+        }
+
+        public Matrix3 Translate_M(float x, float y)
+        {
+            Matrix3 ma = new Matrix3();
+            ma.m[0, 0] = 1.0f; ma.m[1, 0] = 0.0f; ma.m[2, 0] = x;
+            ma.m[0, 1] = 0.0f; ma.m[1, 1] = 1.0f; ma.m[2, 1] = y;
+            ma.m[0, 2] = 0.0f; ma.m[1, 2] = 0.0f; ma.m[2, 2] = 1.0f;
+            return ma;
+        }
+
+        public Matrix3 Rotate_M(float RadAngle)
+        {
+            Matrix3 ma = new Matrix3();
+            ma.m[0, 0] = (float)Math.Cos(RadAngle); ma.m[1, 0] = (float)Math.Sin(RadAngle); ma.m[2, 0] = 0.0f;
+            ma.m[0, 1] = -(float)Math.Sin(RadAngle); ma.m[1, 1] = (float)Math.Cos(RadAngle); ma.m[2, 1] = 0.0f;
+            ma.m[0, 2] = 0.0f; ma.m[1, 2] = 0.0f; ma.m[2, 2] = 1.0f;
+            return ma;
+        }
+
+        public Matrix3 Scale_M(float x, float y)
+        {
+            Matrix3 ma = new Matrix3();
+            ma.m[0, 0] = x; ma.m[1, 0] = 0.0f; ma.m[2, 0] = 0.0f;
+            ma.m[0, 1] = 0.0f; ma.m[1, 1] = y; ma.m[2, 1] = 0.0f;
+            ma.m[0, 2] = 0.0f; ma.m[1, 2] = 0.0f; ma.m[2, 2] = 1.0f;
+            return ma;
+        }
+
+        public Matrix3 Shear_M(float x, float y)
+        {
+            Matrix3 ma = new Matrix3();
+            ma.m[0, 0] = 1.0f; ma.m[1, 0] = x; ma.m[2, 0] = 0.0f;
+            ma.m[0, 1] = y; ma.m[1, 1] = 1.0f; ma.m[2, 1] = 0.0f;
+            ma.m[0, 2] = 0.0f; ma.m[1, 2] = 0.0f; ma.m[2, 2] = 1.0f;
+            return ma;
+        }
+
+        public void MultiplyMatrix(Matrix3 b)
+        {
+            float[,] newM = new float[3,3];
+            for (int c = 0; c < 3; c++)
+                for (int r = 0; r < 3; r++)
+                    newM[c, r] = m[0, r] * b.m[c, 0] + m[1, r] * b.m[c, 1] + m[2, r] * b.m[c, 2];
+            m = newM;
+        }
+
+        public static Matrix3 MultiplyMatrix(Matrix3 a, Matrix3 b)
+        {
+            Matrix3 newMat = new Matrix3();
+            for (int c = 0; c < 3; c++)
+                for (int r = 0; r < 3; r++)
+                    newMat.m[c,r] = a.m[0,r] * b.m[c,0] + a.m[1,r] * b.m[c,1] + a.m[2,r] * b.m[c,2];
+            return newMat;
+        }
+
+        public void Forward(float ix, float iy, out float ox, out float oy)
+        {
+            ox = ix * m[0,0] + iy * m[1,0] + m[2,0];
+            oy = ix * m[0,1] + iy * m[1,1] + m[2,1];
+        }
+
+        public Matrix3 Invert()
+        {
+            Matrix3 matOut = new Matrix3();
+            float det = m[0,0] * (m[1,1] * m[2,2] - m[1,2] * m[2,1]) -
+            m[1,0] * (m[0,1] * m[2,2] - m[2,1] * m[0,2]) +
+            m[2,0] * (m[0,1] * m[1,2] - m[1,1] * m[0,2]);
+
+            float idet = 1.0f / det;
+            matOut.m[0,0] = (m[1,1] * m[2,2] - m[1,2] * m[2,1]) * idet;
+            matOut.m[1,0] = (m[2,0] * m[1,2] - m[1,0] * m[2,2]) * idet;
+            matOut.m[2,0] = (m[1,0] * m[2,1] - m[2,0] * m[1,1]) * idet;
+            matOut.m[0,1] = (m[2,1] * m[0,2] - m[0,1] * m[2,2]) * idet;
+            matOut.m[1,1] = (m[0,0] * m[2,2] - m[2,0] * m[0,2]) * idet;
+            matOut.m[2,1] = (m[0,1] * m[2,0] - m[0,0] * m[2,1]) * idet;
+            matOut.m[0,2] = (m[0,1] * m[1,2] - m[0,2] * m[1,1]) * idet;
+            matOut.m[1,2] = (m[0,2] * m[1,0] - m[0,0] * m[1,2]) * idet;
+            matOut.m[2,2] = (m[0,0] * m[1,1] - m[0,1] * m[1,0]) * idet;
+            return matOut;
+        }
+
+        public bool Similar(Matrix3 matrix)
+        {
+            for (int c = 0; c < 3; c++)
+                for (int r = 0; r < 3; r++)
+                    if (m[c, r] != matrix.m[c, r])
+                        return false;
+
+            return true;
+        }
+
+        #region Operators
+        public static Matrix3 operator +(Matrix3 a, Matrix3 b)
+        {
+            float[,] m = new float[3, 3];
+            m[0, 0] = a.m[0, 0] + b.m[0, 0]; m[1, 0] = a.m[1, 0] + b.m[1, 0]; m[2, 0] = a.m[2, 0] + b.m[2, 0];
+            m[0, 1] = a.m[0, 1] + b.m[0, 1]; m[1, 1] = a.m[1, 1] + b.m[1, 1]; m[2, 1] = a.m[2, 1] + b.m[2, 1];
+            m[0, 2] = a.m[0, 2] + b.m[0, 2]; m[1, 2] = a.m[1, 2] + b.m[1, 2]; m[2, 2] = a.m[2, 2] + b.m[2, 2];
+            return new Matrix3(m);
+        }
+
+        public static Matrix3 operator -(Matrix3 a, Matrix3 b)
+        {
+            float[,] m = new float[3, 3];
+            m[0, 0] = a.m[0, 0] - b.m[0, 0]; m[1, 0] = a.m[1, 0] - b.m[1, 0]; m[2, 0] = a.m[2, 0] - b.m[2, 0];
+            m[0, 1] = a.m[0, 1] - b.m[0, 1]; m[1, 1] = a.m[1, 1] - b.m[1, 1]; m[2, 1] = a.m[2, 1] - b.m[2, 1];
+            m[0, 2] = a.m[0, 2] - b.m[0, 2]; m[1, 2] = a.m[1, 2] - b.m[1, 2]; m[2, 2] = a.m[2, 2] - b.m[2, 2];
+            return new Matrix3(m);
+        }
+
+        public static Matrix3 operator *(Matrix3 a, Matrix3 b)
+        {
+            Matrix3 newMat = new Matrix3();
+            for (int c = 0; c < 3; c++)
+                for (int r = 0; r < 3; r++)
+                    newMat.m[c, r] = a.m[0, r] * b.m[c, 0] + a.m[1, r] * b.m[c, 1] + a.m[2, r] * b.m[c, 2];
+            return newMat;
+        }
+
+        public static Matrix3 operator /(Matrix3 a, Matrix3 b)
+        {
+            Matrix3 newMat = new Matrix3();
+            for (int c = 0; c < 3; c++)
+                for (int r = 0; r < 3; r++)
+                    newMat.m[c, r] = a.m[0, r] * b.m[c, 0] + a.m[1, r] * b.m[c, 1] + a.m[2, r] * b.m[c, 2];
+            return newMat;
+        }
+
+        public static Matrix3 operator %(Matrix3 a, Matrix3 b)
+        {
+            float[,] m = new float[3, 3];
+            m[0, 0] = a.m[0, 0] % b.m[0, 0]; m[1, 0] = a.m[1, 0] % b.m[1, 0]; m[2, 0] = a.m[2, 0] % b.m[2, 0];
+            m[0, 1] = a.m[0, 1] % b.m[0, 1]; m[1, 1] = a.m[1, 1] % b.m[1, 1]; m[2, 1] = a.m[2, 1] % b.m[2, 1];
+            m[0, 2] = a.m[0, 2] % b.m[0, 2]; m[1, 2] = a.m[1, 2] % b.m[1, 2]; m[2, 2] = a.m[2, 2] % b.m[2, 2];
+            return new Matrix3(m);
+        }
+        #endregion
+    }
+}
---
+++ Structures/Vector2.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+namespace ABSoftware.Structures
+{
+    public class Vector2
+    {
+        public static Vector2 Zero { get { return new Vector2(); } }
+        public static Vector2 One { get { return new Vector2(1f, 1f); } }
+        public static Vector2 Right { get { return new Vector2(1f); } }
+        public static Vector2 Up { get { return new Vector2(0f, 1f); } }
+
+        public float x = 0, y = 0;
+
+        public Vector2(float x = 0f, float y = 0f)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public float Length()
+        {
+            return (float)Math.Sqrt(x * x + y * y);
+        }
+
+        public float Distance(Vector2 to)
+        {
+            return (to - this).Length();
+        }
+
+        public Vector2 Normalize()
+        {
+            float l = Length();
+            return new Vector2(x / l, y / l);
+        }
+
+        public float[] GetRotations(Vector2 to)
+        {
+            float sin = (to - this).x / Distance(to);
+            float cos = (to - this).y / Distance(to);
+
+            return new float[] { sin, cos };
+        }
+
+        public void Floor()
+        {
+            x = (int)x;
+            y = (int)y;
+        }
+
+        #region Operators
+        #region Vectors
+        public static Vector2 operator +(Vector2 a, Vector2 b)
+        {
+            return new Vector2(a.x + b.x, a.y + b.y);
+        }
+
+        public static Vector2 operator -(Vector2 a, Vector2 b)
+        {
+            return new Vector2(a.x - b.x, a.y - b.y);
+        }
+
+        public static Vector2 operator *(Vector2 a, Vector2 b)
+        {
+            return new Vector2(a.x * b.x, a.y * b.y);
+        }
+
+        public static Vector2 operator /(Vector2 a, Vector2 b)
+        {
+            return new Vector2(a.x / b.x, a.y / b.y);
+        }
+
+        public static Vector2 operator %(Vector2 a, Vector2 b)
+        {
+            return new Vector2(a.x % b.x, a.y % b.y);
+        }
+
+        public static bool operator >(Vector2 a, Vector2 b)
+        {
+            return (a.x > b.x && a.y > b.y);
+        }
+
+        public static bool operator <(Vector2 a, Vector2 b)
+        {
+            return (a.x < b.x && a.y < b.y);
+        }
+
+        public static bool operator >=(Vector2 a, Vector2 b)
+        {
+            return (a.x >= b.x && a.y >= b.y);
+        }
+
+        public static bool operator <=(Vector2 a, Vector2 b)
+        {
+            return (a.x <= b.x && a.y <= b.y);
+        }
+        #endregion
+
+        #region Floats
+        public static Vector2 operator +(Vector2 a, float b)
+        {
+            return new Vector2(a.x + b, a.y + b);
+        }
+
+        public static Vector2 operator -(Vector2 a, float b)
+        {
+            return new Vector2(a.x - b, a.y - b);
+        }
+
+        public static Vector2 operator *(Vector2 a, float b)
+        {
+            return new Vector2(a.x * b, a.y * b);
+        }
+
+        public static Vector2 operator /(Vector2 a, float b)
+        {
+            return new Vector2(a.x / b, a.y / b);
+        }
+
+        public static Vector2 operator %(Vector2 a, float b)
+        {
+            return new Vector2(a.x % b, a.y % b);
+        }
+
+        public static bool operator >(Vector2 a, float b)
+        {
+            return (a.x > b && a.y > b);
+        }
+
+        public static bool operator <(Vector2 a, float b)
+        {
+            return (a.x < b && a.y < b);
+        }
+
+        public static bool operator >=(Vector2 a, float b)
+        {
+            return (a.x >= b && a.y >= b);
+        }
+
+        public static bool operator <=(Vector2 a, float b)
+        {
+            return (a.x <= b && a.y <= b);
+        }
+        #endregion
+        #endregion
+
+        public override string ToString()
+        {
+            return "(" + this.x + ", " + this.y + ")";
+        }
+    }
+}
