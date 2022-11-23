db0c3b98e2ac31d9583899b41a990fb4d54f804a
Alex <75645403+RealABlair@users.noreply.github.com>
2022-11-24 02:24:04 +0300

    Update Matrix3.cs

--- Structures/Matrix3.cs
+++ Structures/Matrix3.cs
@@ -13,42 +13,52 @@ namespace ABSoftware.Structures
 
         public Matrix3(float[,] m)
         {
-            this.m = m;
+            int Rows = m.GetLength(1);
+            int Columns = m.GetLength(0);
+            float[,] editedMatrix = new float[Rows, Columns];
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c < Columns; c++)
+                {
+                    editedMatrix[r, c] = m[c, r];
+                }
+            }
+            this.m = editedMatrix;
         }
 
         public void Identity()
         {
-            m[0,0] = 1.0f; m[1,0] = 0.0f; m[2,0] = 0.0f;
-            m[0,1] = 0.0f; m[1,1] = 1.0f; m[2,1] = 0.0f;
-            m[0,2] = 0.0f; m[1,2] = 0.0f; m[2,2] = 1.0f;
+            m[0, 0] = 1.0f; m[1, 0] = 0.0f; m[2, 0] = 0.0f;
+            m[0, 1] = 0.0f; m[1, 1] = 1.0f; m[2, 1] = 0.0f;
+            m[0, 2] = 0.0f; m[1, 2] = 0.0f; m[2, 2] = 1.0f;
         }
 
         public void Translate(float x, float y)
         {
-            m[0,0] = 1.0f; m[1,0] = 0.0f; m[2,0] = x;
-            m[0,1] = 0.0f; m[1,1] = 1.0f; m[2,1] = y;
-            m[0,2] = 0.0f; m[1,2] = 0.0f; m[2,2] = 1.0f;
+            m[0, 0] = 1.0f; m[1, 0] = 0.0f; m[2, 0] = x;
+            m[0, 1] = 0.0f; m[1, 1] = 1.0f; m[2, 1] = y;
+            m[0, 2] = 0.0f; m[1, 2] = 0.0f; m[2, 2] = 1.0f;
         }
 
         public void Rotate(float RadAngle)
         {
-            m[0,0] = (float)Math.Cos(RadAngle); m[1,0] = (float)Math.Sin(RadAngle); m[2,0] = 0.0f;
-            m[0,1] = -(float)Math.Sin(RadAngle); m[1,1] = (float)Math.Cos(RadAngle); m[2,1] = 0.0f;
-            m[0,2] = 0.0f; m[1,2] = 0.0f; m[2,2] = 1.0f;
+            m[0, 0] = (float)Math.Cos(RadAngle); m[1, 0] = (float)Math.Sin(RadAngle); m[2, 0] = 0.0f;
+            m[0, 1] = -(float)Math.Sin(RadAngle); m[1, 1] = (float)Math.Cos(RadAngle); m[2, 1] = 0.0f;
+            m[0, 2] = 0.0f; m[1, 2] = 0.0f; m[2, 2] = 1.0f;
         }
 
         public void Scale(float x, float y)
         {
-            m[0,0] = x; m[1,0] = 0.0f; m[2,0] = 0.0f;
-            m[0,1] = 0.0f; m[1,1] = y; m[2,1] = 0.0f;
-            m[0,2] = 0.0f; m[1,2] = 0.0f; m[2,2] = 1.0f;
+            m[0, 0] = x; m[1, 0] = 0.0f; m[2, 0] = 0.0f;
+            m[0, 1] = 0.0f; m[1, 1] = y; m[2, 1] = 0.0f;
+            m[0, 2] = 0.0f; m[1, 2] = 0.0f; m[2, 2] = 1.0f;
         }
 
         public void Shear(float x, float y)
         {
-            m[0,0] = 1.0f; m[1,0] = x; m[2,0] = 0.0f;
-            m[0,1] = y; m[1,1] = 1.0f; m[2,1] = 0.0f;
-            m[0,2] = 0.0f; m[1,2] = 0.0f; m[2,2] = 1.0f;
+            m[0, 0] = 1.0f; m[1, 0] = x; m[2, 0] = 0.0f;
+            m[0, 1] = y; m[1, 1] = 1.0f; m[2, 1] = 0.0f;
+            m[0, 2] = 0.0f; m[1, 2] = 0.0f; m[2, 2] = 1.0f;
         }
 
         public Matrix3 Identity_M()
@@ -88,7 +98,7 @@ namespace ABSoftware.Structures
 
         public void MultiplyMatrix(Matrix3 b)
         {
-            float[,] newM = new float[3,3];
+            float[,] newM = new float[3, 3];
             for (int c = 0; c < 3; c++)
                 for (int r = 0; r < 3; r++)
                     newM[c, r] = m[0, r] * b.m[c, 0] + m[1, r] * b.m[c, 1] + m[2, r] * b.m[c, 2];
@@ -100,33 +110,33 @@ namespace ABSoftware.Structures
             Matrix3 newMat = new Matrix3();
             for (int c = 0; c < 3; c++)
                 for (int r = 0; r < 3; r++)
-                    newMat.m[c,r] = a.m[0,r] * b.m[c,0] + a.m[1,r] * b.m[c,1] + a.m[2,r] * b.m[c,2];
+                    newMat.m[c, r] = a.m[0, r] * b.m[c, 0] + a.m[1, r] * b.m[c, 1] + a.m[2, r] * b.m[c, 2];
             return newMat;
         }
 
         public void Forward(float ix, float iy, out float ox, out float oy)
         {
-            ox = ix * m[0,0] + iy * m[1,0] + m[2,0];
-            oy = ix * m[0,1] + iy * m[1,1] + m[2,1];
+            ox = ix * m[0, 0] + iy * m[1, 0] + m[2, 0];
+            oy = ix * m[0, 1] + iy * m[1, 1] + m[2, 1];
         }
 
         public Matrix3 Invert()
         {
             Matrix3 matOut = new Matrix3();
-            float det = m[0,0] * (m[1,1] * m[2,2] - m[1,2] * m[2,1]) -
-            m[1,0] * (m[0,1] * m[2,2] - m[2,1] * m[0,2]) +
-            m[2,0] * (m[0,1] * m[1,2] - m[1,1] * m[0,2]);
+            float det = m[0, 0] * (m[1, 1] * m[2, 2] - m[1, 2] * m[2, 1]) -
+            m[1, 0] * (m[0, 1] * m[2, 2] - m[2, 1] * m[0, 2]) +
+            m[2, 0] * (m[0, 1] * m[1, 2] - m[1, 1] * m[0, 2]);
 
             float idet = 1.0f / det;
-            matOut.m[0,0] = (m[1,1] * m[2,2] - m[1,2] * m[2,1]) * idet;
-            matOut.m[1,0] = (m[2,0] * m[1,2] - m[1,0] * m[2,2]) * idet;
-            matOut.m[2,0] = (m[1,0] * m[2,1] - m[2,0] * m[1,1]) * idet;
-            matOut.m[0,1] = (m[2,1] * m[0,2] - m[0,1] * m[2,2]) * idet;
-            matOut.m[1,1] = (m[0,0] * m[2,2] - m[2,0] * m[0,2]) * idet;
-            matOut.m[2,1] = (m[0,1] * m[2,0] - m[0,0] * m[2,1]) * idet;
-            matOut.m[0,2] = (m[0,1] * m[1,2] - m[0,2] * m[1,1]) * idet;
-            matOut.m[1,2] = (m[0,2] * m[1,0] - m[0,0] * m[1,2]) * idet;
-            matOut.m[2,2] = (m[0,0] * m[1,1] - m[0,1] * m[1,0]) * idet;
+            matOut.m[0, 0] = (m[1, 1] * m[2, 2] - m[1, 2] * m[2, 1]) * idet;
+            matOut.m[1, 0] = (m[2, 0] * m[1, 2] - m[1, 0] * m[2, 2]) * idet;
+            matOut.m[2, 0] = (m[1, 0] * m[2, 1] - m[2, 0] * m[1, 1]) * idet;
+            matOut.m[0, 1] = (m[2, 1] * m[0, 2] - m[0, 1] * m[2, 2]) * idet;
+            matOut.m[1, 1] = (m[0, 0] * m[2, 2] - m[2, 0] * m[0, 2]) * idet;
+            matOut.m[2, 1] = (m[0, 1] * m[2, 0] - m[0, 0] * m[2, 1]) * idet;
+            matOut.m[0, 2] = (m[0, 1] * m[1, 2] - m[0, 2] * m[1, 1]) * idet;
+            matOut.m[1, 2] = (m[0, 2] * m[1, 0] - m[0, 0] * m[1, 2]) * idet;
+            matOut.m[2, 2] = (m[0, 0] * m[1, 1] - m[0, 1] * m[1, 0]) * idet;
             return matOut;
         }
 
