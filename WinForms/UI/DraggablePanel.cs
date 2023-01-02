26bf935698abab63928b5cbc29ab27e88b445103
Alex <75645403+RealABlair@users.noreply.github.com>
2023-01-02 23:52:26 +0300

    Update DraggablePanel.cs

--- WinForms/UI/DraggablePanel.cs
+++ WinForms/UI/DraggablePanel.cs
@@ -5,13 +5,12 @@ namespace ABSoftware.UI
 {
     public class DraggablePanel : Panel
     {
-
         public DraggablePanel() : base()
         {
-            this.MouseDown += Drag;
+
         }
 
-        private void Drag(object sender, MouseEventArgs e)
+        protected override void OnMouseDown(MouseEventArgs e)
         {
             this.Capture = false;
             Message m = Message.Create(FindForm().Handle, 0xa1, new IntPtr(2), IntPtr.Zero);
