e6455c97faa9f5454edbf5602f801f1e6f6da187
Alex <75645403+RealABlair@users.noreply.github.com>
2023-01-04 17:23:02 +0300

    Add files via upload

---
+++ ABRandom.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ABSoftware
+{
+    public class ABRandom
+    {
+        public uint Seed { get; set; }
+
+        public ABRandom()
+        {
+            this.Seed = (uint)DateTime.Now.Ticks;
+        }
+
+        public ABRandom(uint Seed)
+        {
+            this.Seed = Seed;
+        }
+
+        private uint GetRandom()
+        {
+            this.Seed += 0xA5F23012;
+            ulong buffer;
+            buffer = (ulong)this.Seed * 0x15FABDA0;
+            uint block1 = (uint)((buffer >> 32) ^ buffer);
+            buffer = (ulong)block1 * 0x09BAFD01;
+            uint block2 = (uint)((buffer >> 32) ^ buffer);
+            return block2;
+        }
+
+        public int GetRandomInt(int min = int.MinValue, int max = int.MaxValue)
+        {
+            return (int)(GetRandom() % (max - min)) + min;
+        }
+
+        public double GetRandomDouble()
+        {
+            return (double)(GetRandom() / (double)uint.MaxValue);
+        }
+
+        public void RandomizeBytes(byte[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = (byte)(GetRandom() % 256);
+            }
+        }
+    }
+}
