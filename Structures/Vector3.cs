f39996b6fd7a34d03890b1e81180717352291dca
Alex <75645403+RealABlair@users.noreply.github.com>
2023-04-08 08:59:48 +0300

    Update Vector3.cs

--- Structures/Vector3.cs
+++ Structures/Vector3.cs
@@ -121,7 +121,7 @@ namespace ABSoftware.Structures
 
         public static bool operator >(Vector3 a, Vector3 b)
         {
-            return (a.x > b.x && a.y > b.y && a.z <= b.z);
+            return (a.x > b.x && a.y > b.y && a.z > b.z);
         }
 
         public static bool operator <(Vector3 a, Vector3 b)
