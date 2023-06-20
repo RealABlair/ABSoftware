4eacb004314ad92d1048abe13c38398933122520
Alex <75645403+RealABlair@users.noreply.github.com>
2023-06-20 04:21:07 +0300

    Update Hash.cs

--- Hash.cs
+++ Hash.cs
@@ -7,50 +7,9 @@ namespace ABSoftware
         public static int BLOCK_SIZE = 64;
         public static int BlockRange { get { return BLOCK_SIZE / 4; } }
         
-        public static string GetHash(string input)
+        public static string GetHash(string text, System.Text.Encoding encoding)
         {
-            uint A = 0x00010203;
-            uint B = 0x04050607;
-            uint C = 0x08090A0B;
-            uint D = 0x0C0D0E0F;
-
-            int inputLength = RoundToClosest(input.Length, BLOCK_SIZE);
-            input = input.PadRight(inputLength, '\0');
-
-            char[] buffer;
-
-            for(int i = 0; i < inputLength / BLOCK_SIZE; i++)
-            {
-                buffer = input.Substring(i * BLOCK_SIZE, BLOCK_SIZE).ToCharArray();
-
-                for(int c = 0; c < buffer.Length; c++)
-                {
-                    uint F = 0;
-                    if(c >= 0 && c <= BlockRange)
-                    {
-                        F = (uint)(buffer[c] & RotateLeft(buffer[c + 1], 6)) ^ 1;
-                    }
-                    else if(c > BlockRange && c <= BlockRange * 2)
-                    {
-                        F = (uint)(buffer[c] & buffer[c - 1] & ~buffer[c + 1]) ^ 1;
-                    }
-                    else if (c > BlockRange * 2 && c <= BlockRange * 3)
-                    {
-                        F = (uint)(~buffer[c] ^ buffer[c - 1]) ^ 1;
-                    }
-                    else if (c > BlockRange * 3 && c < BLOCK_SIZE)
-                    {   
-                        F = (uint)(buffer[c] & ~D ^ 1 << 31);
-                    }
-                    F = RotateLeft(F + A + buffer[c], 3);
-                    A = RotateLeft(D, 3);
-                    D = RotateLeft(C, 7);
-                    C = RotateLeft(B, 1);
-                    B = B + RotateLeft(F, 7);
-                }
-            }
-
-            return A.ToString("x8") + B.ToString("x8") + C.ToString("x8") + D.ToString("x8");
+            return GetHash(encoding.GetBytes(text));
         }
 
         public static string GetHash(byte[] input)
