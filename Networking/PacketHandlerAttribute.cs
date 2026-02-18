3a74edf1cdd27a60e1239408ca9a5c770fc9de18
Alex <75645403+RealABlair@users.noreply.github.com>
2026-02-18 16:57:30 +0300

    Add files via upload

---
+++ Networking/PacketHandlerAttribute.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection;
+
+namespace ABSoftware.Networking
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = false)]
+    public class PacketHandlerAttribute : Attribute
+    {
+        public PacketHandlerAttribute(Type packetType)
+        {
+            this.packetType = packetType;
+        }
+
+        public Type packetType;
+    }
+
+    public static class PacketHandlerAttributeUtils
+    {
+        public static PacketHandlerAttribute GetPacketHandlerAttribute(this MethodInfo method)
+        {
+            PacketHandlerAttribute[] attributes = (PacketHandlerAttribute[])method.GetCustomAttributes(typeof(PacketHandlerAttribute), false);
+            if (attributes.Length > 0)
+                return attributes[0];
+
+            return null;
+        }
+
+        public static PacketHandlerAttribute[] GetPacketHandlerAttributes(this MethodInfo method)
+        {
+            PacketHandlerAttribute[] attributes = (PacketHandlerAttribute[])method.GetCustomAttributes(typeof(PacketHandlerAttribute), false);
+
+            return attributes;
+        }
+    }
+
+    public struct PacketHandlerData
+    {
+        public Type packetType;
+        public ArrayList<(MethodInfo, object)> packetHandlers;
+
+        public PacketHandlerData(Type packetType)
+        {
+            this.packetType = packetType;
+            this.packetHandlers = new ArrayList<(MethodInfo, object)>();
+        }
+    }
+}
